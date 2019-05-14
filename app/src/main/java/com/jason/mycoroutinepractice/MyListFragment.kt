7ecec86dcdb1fe01379c6659917c0c76f0b73116
JasonChien <s124202468@gmail.com>
2019-05-14 10:57:47 +0800

    Add parallel, sequentially experimental fragment.

--- app/src/main/java/com/jason/mycoroutinepractice/MyListFragment.kt
+++ app/src/main/java/com/jason/mycoroutinepractice/MyListFragment.kt
@@ -5,18 +5,26 @@ import android.support.v4.app.Fragment
 import android.support.v4.app.ListFragment
 import android.widget.ArrayAdapter
 import com.jason.mycoroutinepractice.lesson.LaunchFragment
+import com.jason.mycoroutinepractice.lesson.LaunchParallelFragment
+import com.jason.mycoroutinepractice.lesson.LaunchSequentiallyFragment
 
 class MyListFragment : ListFragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        val list = arrayListOf(LESSON_LAUNCH)
+        val list = arrayListOf(
+            LESSON_LAUNCH,
+            LESSON_SEQUENTIALLY,
+            LESSON_PARALLEL)
 
         listAdapter = ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, list)
         listView.setOnItemClickListener { _, _, position, _ ->
             when (list[position]) {
                 LESSON_LAUNCH -> showFragment(LaunchFragment(), LaunchFragment.TAG)
+                LESSON_SEQUENTIALLY -> showFragment(LaunchSequentiallyFragment(), LaunchSequentiallyFragment.TAG)
+                LESSON_PARALLEL -> showFragment(LaunchParallelFragment(), LaunchParallelFragment.TAG)
+
             }
         }
     }
@@ -31,8 +39,7 @@ class MyListFragment : ListFragment() {
     companion object {
         const val TAG = "SampleListFragment"
         private const val LESSON_LAUNCH = "1. Launch Coroutine"
+        private const val LESSON_SEQUENTIALLY = "2. Launch Coroutine Sequentially"
+        private const val LESSON_PARALLEL = "3. Launch Coroutine Parallel"
     }
-
-
-
 }
